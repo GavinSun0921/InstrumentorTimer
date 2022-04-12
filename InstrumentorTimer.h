110a8260b3a227a5afca6ef784d86ad10f58c052
GavinSun0921 <gavinsun0921@foxmail.com>
2022-04-12 22:39:40 +0800

    with right code

--- InstrumentorTimer.h
+++ InstrumentorTimer.h
@@ -1,35 +1,143 @@
-#include <iostream>
+//
+// Basic instrumentation profiler by Cherno
+// ------------------------
+// Modified by Gavin
+// 1. Make 'WriteProfile' thread-safe by add 'std::mutex' and 'std::lock_guard'
+// 2. Remove the 'InstrumentationSession' which is no significance
+// 3. Add destructor for 'Instrumentor'
+// 4. Change the Constructor of 'Instrumentor' to private
+// ------------------------
 
-#include "InstrumentorTimer.h"
-#include "InstrumentorMacro.h"
+// Usage: include this header file somewhere in your code (e.g. precompiled header), and then use like:
+//
+// Instrumentor::Get().BeginSession("Session Name");        // Begin session 
+// {
+//     InstrumentationTimer timer("Profiled Scope Name");   // Place code like this in scopes you'd like to include in profiling
+//     // Code
+// }
+// Instrumentor::Get().EndSession();                        // End Session
+//
+// You will probably want to macro-fy this, to switch on/off easily and use things like __FUNCSIG__ for the profile name.
+//
+#pragma once
 
-namespace Benchmark {
-    int Fibonacci(int x) {
-        std::string name = std::string("Fibonacci ") + std::to_string(x);
-        PROFILE_SCOPE(name.c_str());
+#include <string>
+#include <chrono>
+#include <fstream>
+#include <thread>
 
-        if (x < 3) return 1;
-        std::cout << "not finished" << std::endl;
-        int part1 = Fibonacci(x - 1);
-        int part2 = Fibonacci(x - 2);
-        return part1 + part2;
+struct ProfileResult {
+    std::string Name;
+    long long Start, End;
+    uint64_t ThreadID;
+};
+
+class Instrumentor {
+private:
+    std::string m_SessionName;
+    std::ofstream m_OutputStream;
+    int m_ProfileCount;
+    std::mutex m_Lock;
+    bool m_ActiveSession;
+
+private:
+    Instrumentor()
+            : m_SessionName("None"), m_ProfileCount(0), m_ActiveSession(false) {
     }
 
-    void RunBenchmarks() {
-        PROFILE_FUNCTION();
-
-        std::cout << "Running Benchmarks..." << std::endl;
-        std::thread t1([]() { Fibonacci(9); });
-        std::thread t2([]() { Fibonacci(10); });
-
-        t1.join();
-        t2.join();
+public:
+    static Instrumentor &Instance() {
+        static Instrumentor instance;
+        return instance;
     }
-}
 
-int main() {
-    Instrumentor::Instance().BeginSession("Profile");
-    Benchmark::RunBenchmarks();
-    Instrumentor::Instance().EndSession();
-    return 0;
-}
+    ~Instrumentor() {
+        EndSession();
+    }
+
+    void BeginSession(const std::string &name, const std::string &filepath = "results.json") {
+        if (m_ActiveSession) {
+            EndSession();
+        }
+        m_ActiveSession = true;
+        m_SessionName = name;
+        m_OutputStream.open(filepath);
+        WriteHeader();
+    }
+
+    void EndSession() {
+        if (!m_ActiveSession) {
+            return;
+        }
+        m_ActiveSession = false;
+        WriteFooter();
+        m_OutputStream.close();
+        m_ProfileCount = 0;
+    }
+
+    void WriteProfile(const ProfileResult &result) {
+        std::lock_guard<std::mutex> lockGuard(m_Lock);
+
+        if (m_ProfileCount++ > 0)
+            m_OutputStream << ",";
+
+        std::string name = result.Name;
+        std::replace(name.begin(), name.end(), '"', '\'');
+
+        m_OutputStream << "{";
+        m_OutputStream << "\"cat\":\"function\",";
+        m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
+        m_OutputStream << "\"name\":\"" << name << "\",";
+        m_OutputStream << "\"ph\":\"X\",";
+        m_OutputStream << "\"pid\":\"Main\",";
+        m_OutputStream << "\"tid\":" << result.ThreadID << ",";
+        m_OutputStream << "\"ts\":" << result.Start;
+        m_OutputStream << "}";
+
+        m_OutputStream.flush();
+    }
+
+    void WriteHeader() {
+        m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+        m_OutputStream.flush();
+    }
+
+    void WriteFooter() {
+        m_OutputStream << "]}";
+        m_OutputStream.flush();
+    }
+};
+
+class InstrumentationTimer {
+private:
+    const char* m_Name;
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+    bool m_Stopped;
+
+public:
+    explicit InstrumentationTimer(const char* name)
+            : m_Name(name), m_Stopped(false) {
+        m_StartTimepoint = std::chrono::high_resolution_clock::now();
+    }
+
+    ~InstrumentationTimer() {
+        if (!m_Stopped){
+            Stop();
+        }
+    }
+
+    void Stop() {
+        auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+        long long start = std::chrono::time_point_cast<std::chrono::microseconds>(
+                m_StartTimepoint).time_since_epoch().count();
+        long long end = std::chrono::time_point_cast<std::chrono::microseconds>(
+                endTimepoint).time_since_epoch().count();
+
+        uint64_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
+        Instrumentor::Instance().WriteProfile({m_Name, start, end, threadID});
+
+        m_Stopped = true;
+    }
+};
