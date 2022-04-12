13c2d78750626c41c054a1ca89617331f292a8bd
GavinSun0921 <gavinsun0921@foxmail.com>
2022-04-12 22:30:39 +0800

    add InstrumentorMacro

---
+++ InstrumentorMacro.h
@@ -0,0 +1,11 @@
+#define PROFILING 1
+#if PROFILING
+#define PROFILE_SCOPE(name) InstrumentationTimer timer##__LINE__(name)
+#ifdef _MSC_VER
+#define PROFILE_FUNCTION() PROFILE_SCOPE(__FUNCSIG__)
+#else
+#define PROFILE_FUNCTION() PROFILE_SCOPE(__PRETTY_FUNCTION__)
+#endif
+#else
+#define PROFILE_SCOPE(name)
+#endif
